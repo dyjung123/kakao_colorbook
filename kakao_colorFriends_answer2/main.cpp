020fc51076e2c99582c1de70ab620f5a960bd66a
dongryun jeong <jeongdongryun@gmail.com>
2018-04-02 23:55:00 +0900

    Initial Commit

---
+++ kakao_colorFriends_answer2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  kakao_colorFriends_answer2
+//
+//  Created by dongryun jeong on 2018. 4. 2..
+//  Copyright © 2018년 dongryun jeong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
